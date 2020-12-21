9f697844bd22bd775ebbf86db876a11bed3fd6f6
Omar Gatica <omar.gatica.017@gmail.com>
2020-12-21 14:00:16 -0800

    Finsished skeleton, need to update with projects and styling.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Projects from './Projects';
+import SocialProfiles from './SocialProfiles'; 
+import profile from './assets/profile.png'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+	state = { displayBio: false };
+
+	toggleDisplayBio = () => {
+		this.setState({displayBio: !this.state.displayBio});
+	}
+
+    render() {
+        return (
+            <div>
+                <img src={profile} alt='profile' className="profile"/>
+                <h1>Hello!</h1>
+                <p>My name is Omar. I'm a software engineer.</p>
+                <p>I'm always looking forward to working on meaningful projects.</p>
+                {
+                    this.state.displayBio ? (
+                        <div>
+                            <p>I live in Downey and enjoy coding every day.</p>
+                            <p>My favorite languages are Python and JavaScript.</p>
+                            <p>Besides coding, I also love dicovering new music and spending time with my dog Jimmy.</p>
+							<div>
+								<button onClick={this.toggleDisplayBio}>Show Less</button>
+							</div>
+                        </div>
+                     ) : (
+						 <div>
+							 <button onClick={this.toggleDisplayBio}>Read more</button>
+						 </div>
+					 )
+                }
+				<hr />
+				<Projects />
+				<hr />
+				<SocialProfiles />
+            </div>
+        )
+    }
 }
 
 export default App;
---
+++ src/data/socialProfiles.js
@@ -0,0 +1,29 @@
+import emailIcon from '../assets/email_icon.png';
+import githubIcon from '../assets/github_icon.png';
+import linkedinIcon from '../assets/linkedin_icon.png';
+import twitterIcon from '../assets/twitter_icon.png';
+
+const SOCIAL_PROFILES = [
+    {
+        id: 1,
+        link: 'mailto:omar.gatica.017@gmail.com',
+        image: emailIcon
+    },
+    {
+        id: 2,
+        link: 'https://github.com/ogatica017',
+        image: githubIcon
+    },
+    {
+        id: 3,
+        link: 'https://www.linkedin.com/in/omargatica17/',
+        image: linkedinIcon
+    },
+    {
+        id: 4,
+        link: 'https://twitter.com/ogatica017',
+        image: twitterIcon
+    }
+];
+
+export default SOCIAL_PROFILES;
