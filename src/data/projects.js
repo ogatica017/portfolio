450581074cc03d039de82d8643edd29ba6777ad4
Omar Gatica <omar.gatica.017@gmail.com>
2020-12-21 22:28:36 -0800

    Implemented react app from scratch. Updated projects.

--- src/data/projects.js
+++ src/data/projects.js
@@ -5,23 +5,23 @@ import project3 from '../assets/project3.png';
 const PROJECTS = [
     {
         id: 1,
-        title: 'Example React Application',
-        description: 'A React App that I built, involving JS and core web dev concepts!',
-        link: 'https://github.com/15Dkatz/example',
+        title: 'Gitlet: A Git Clone',
+        description: 'A lighter version-control system with all the local capabilities of Git, allowing users to save accessible backups of previous versions of a file, locally preventing potential data loss while being privacy-friendly',
+        link: 'https://github.com/ogatica017/gitlet',
         image: project1
     },
     {
         id: 2,
-        title: 'My API',
-        description: 'A REST API that I built from scratch with GET and POST requests!',
-        link: 'https://github.com/15Dkatz/example',
+        title: 'Bearmaps',
+        description: 'Manipulated real-world mapping data from Google Maps API to create the backend of a mapping application that generates an image of a specified region and finds the optimal route between two locations using A* search',
+        link: 'https://github.com/ogatica017/bearmaps',
         image: project2
     },
     {
         id: 3,
-        title: 'Operating Systems Final Project',
-        description: 'My unique final project for my university Operating Systems course.',
-        link: 'https://github.com/15Dkatz/example',
+        title: 'Reinforcement Learning Pac-Man',
+        description: 'Implemented Q-learning which enabled a Pac-Man agent to use reinforcement learning and search for an optimal policy to a Markov Decision Process through trial and error, learning to avoid ghosts while maximizing game score',
+        link: 'https://github.com/ogatica017/rl_pacman',
         image: project3
     }
 ];
